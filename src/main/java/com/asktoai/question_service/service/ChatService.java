391b28a1992889ecbc823fb6fa13ad5c81d49974
Anil Oeguetveren <oegueta@emea.corpdir.net>
2024-05-08 15:32:26 +0200

    Excetions are added to supplyAsync

--- src/main/java/com/asktoai/question_service/service/ChatService.java
+++ src/main/java/com/asktoai/question_service/service/ChatService.java
@@ -9,6 +9,7 @@ import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -31,7 +32,8 @@ public class ChatService {
 
 
     /**
-     * This method is used to fetch the answer from AI using CompletableFuture with supplyAsync
+     * This method is used to fetch the answer from AI using CompletableFuture with supplyAsync.
+     * This is more elegant way to create CompletableFuture.
      * Burada daha iyi bir örnek olusturmak icin supplyAsync kullanilarak verilmistir.
      * Asagidaki methodda (fetchAnswerFromAIWithBasicCompletableFuture) sadece simple CompletableFuture kullanilarak verilmistir.
      * @return CompletableFuture<Answer>
@@ -44,17 +46,21 @@ public class ChatService {
             log.info("Getting answer from Gemini");
             CommonUtils.sleep(Duration.ofSeconds(2));
             return new Answer(1, question, "Here is your answer to your question");
-        }, executor).exceptionally(ex -> {
-            log.error("Error occurred while fetching answer from AI", ex);
-            return new Answer(1, question, "Sorry, I am unable to fetch the answer at the moment");
-        });
+        }, executor)
+                .exceptionally(ex -> {
+                    log.error("Error occurred while fetching answer from AI", ex);
+                    return new Answer(1, question, "Sorry, I am unable to fetch the answer at the moment");
+        })      .orTimeout(10, TimeUnit.SECONDS)
+                .exceptionally(ex -> {
+                    log.error("Timeout occurred while fetching answer from AI", ex);
+                    return new Answer(1, question, "Sorry, I am unable to fetch the answer at the moment. Reason: timeout occurred");
+                });
     }
 
     /**
      * This method is used to fetch the answer from AI using Simple CompletableFuture
      * Bu sadece complatable Future kullanilarak verilmis bir örnek method.
      * Diger method da (supplyAnswerFromAI) supplyAsync kullanilarak daha iyi bir örnek verilmistir.
-     *
      * @return CompletableFuture<Answer>
      */
     public CompletableFuture<Answer> fetchAnswerFromAIWithBasicCompletableFuture(String question) {
